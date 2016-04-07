cde5b15645317860d18c962ebac866a8a4d0aac6
cristianocs <engcristianoc@gmail.com>
2016-04-07 09:16:58 -0300

    Adding test for multiplication and division

--- ClassCalculator.py
+++ ClassCalculator.py
@@ -3,5 +3,11 @@ class ClassCalculadora():
 def sum(self,x, y):
     return x+y
     
-def sub(self,x, y):
+def subtraction(self,x, y):
     return x-y
+    
+def multiplication(self,x, y):
+    return x*y
+    
+def division(self,x, y):
+    return x/y
--- app/Calculator/test_calculator.py
+++ app/Calculator/test_calculator.py
@@ -7,7 +7,16 @@ def test_calculator_sum():
       calc = ClassCalculator
       assert calc.sum(2,4) = 5
 
-def test_calculadora_sub():
+def test_calculadora_subtraction():
       calc = ClassCalculator
-      assert calc.sub(5,3) = 2
+      assert calc.subtration(5,3) = 2
+
+def test_calculadora_multiplication():
+      calc = ClassCalculator
+      assert calc.multiplication(5,3) = 15
+      
+def test_calculadora_division():
+      calc = ClassCalculator
+      assert calc.division(6,3) = 2
+      
 
