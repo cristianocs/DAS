fd9ab6740c98bad28457e3febe46814b4d5570e1
xxxxx <xxxx.@gmail.com>
2016-04-07 09:00:21 -0300

    Creating app and Class Calculator

---
+++ app/Calculator/ClassCalculator.py
@@ -0,0 +1,4 @@
+class ClassCalculadora():
+
+def adicao(self,x, y):
+    pass
---
+++ app/Calculator/calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import pytest
+
+from app.Calculator import ClassCalculator
+
+def test_calculadora():
+      calc = ClassCalculator
+      assert calc.adicao(2,4) = 5
+      
