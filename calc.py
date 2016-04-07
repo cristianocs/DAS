86f5226ffa7363b9b75d4e9a39dfc19f5fdf4515
xxxxx <xxxx.@gmail.com>
2016-04-07 08:57:00 -0300

    Creating test for sum

--- calc.py
+++ calc.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import pytest
 
+from app.Calculator import ClassCalculator
+
 def test_calculadora():
-    assert True
+      calc = ClassCalculator
+      assert calc.adicao(2,4) = 5
+      
