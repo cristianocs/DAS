a028fe0a84572d0ccf5b0012aa8fff2271a8378f
cristianocs <engcristianoc@gmail.com>
2016-06-15 21:54:38 -0300

    Modifying routes, index

--- post/views.py
+++ post/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import View
+from django.views.generic import CreateView, ListView
+from .models import Post
 
-# Create your views here.
+class PostView(View):
+	def post_list(request):
+		posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('data_publicacao')
+		return render(request, 'post.html', {'posts': posts})
